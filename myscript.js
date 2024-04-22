3f713c09e02c7e003c910b066bd4f5b3f0e608ff
mpachonm <mpachonm@unal.edu.co>
2024-04-22 18:50:02 -0500

    Edit grid to be made using flexbox

--- myscript.js
+++ myscript.js
@@ -1,12 +1,10 @@
 const container = document.getElementById("container");
 
 function makeGrid(rows,columns){
-container.style.setProperty("--grid-rows",rows);
-container.style.setProperty("--grid-cols",columns);
 for (c = 0; c < (rows * columns); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
+    cell.innerText = (c+1);
+    container.appendChild(cell);
 };
 };
 makeGrid(16,16);
