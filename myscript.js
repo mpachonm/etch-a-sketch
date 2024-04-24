f2c7ae2348cf8755720b477237cc5bb8dedecdb5
mpachonm <mpachonm@unal.edu.co>
2024-04-24 18:41:19 -0500

    Fix issue where the grid was 15 columns by 16 rows, not 16x16 as intended

--- myscript.js
+++ myscript.js
@@ -1,9 +1,9 @@
 const container = document.getElementById("container");
 
 function makeGrid(rows,columns){
-for (c = 0; c < (rows * columns); c++) {
+for (c = 0; c <(rows * columns+columns); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c+1);
+    /*cell.innerText = (c+1);*/
     container.appendChild(cell);
 };
 };
