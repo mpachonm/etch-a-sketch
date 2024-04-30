1e6e7c6541b7ae3edd3f323064e66068c1377e09
mpachonm <mpachonm@unal.edu.co>
2024-04-30 17:51:09 -0500

    Fix hover so that the colour stays after unhovering

--- myscript.js
+++ myscript.js
@@ -1,10 +1,15 @@
 const container = document.getElementById("container");
 
+function changeBackgroundColor(){
+    this.style.backgroundColor='yellow';
+    };
+
 function makeGrid(rows,columns){
 for (c = 0; c <(rows * columns+columns); c++) {
     let cell = document.createElement("div");
     /*cell.innerText = (c+1);*/
     container.appendChild(cell);
+    cell.addEventListener("mouseover",changeBackgroundColor);
 };
 };
 makeGrid(16,16);
